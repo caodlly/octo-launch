5291b745ad391cde95b4a910ac0a9f92c76c55a4
Wissam Al Kahwaji <wissamalkahwaji@gmail.com>
2024-04-23 11:03:54 +0200

    chore: fix format

--- apps/authentications/serializers/__init__.py
+++ apps/authentications/serializers/__init__.py
@@ -1,5 +1,10 @@
 from .LoginSerializer import LoginSerializer  # noqa: F401
-from .UserSerializer import UserSerializer, UserSerializerPrivate, UserSerializerPublic, UserStatusAuth  # noqa: F401
+from .UserSerializer import (
+    UserSerializer,  # noqa: F401
+    UserSerializerPrivate,  # noqa: F401
+    UserSerializerPublic,  # noqa: F401
+    UserStatusAuth,  # noqa: F401
+)
 from .RegisterSerializer import RegisterSerializer  # noqa: F401
 from .UpdateUserSerializer import UpdateUserSerializer  # noqa: F401
 from .VerificationCodeSerializer import VerificationCodeSerializer  # noqa: F401
