dac47e1e61d1af225395649499ccf01091e89bca
Wissam Al-Kahwaji <wissamalkahwaji@gmail.com>
2024-05-14 06:27:14 +0300

    feat: add factories

---
+++ apps/users/factories.py
@@ -0,0 +1,28 @@
+import factory
+from .models import User
+from faker import Faker
+from django.contrib.auth.hashers import make_password
+
+password = Faker().password()
+
+
+class UserFactory(factory.Factory):
+    class Meta:
+        model = User
+
+    name = Faker().name()
+    email = Faker().email()
+    username = Faker().user_name()[0:10]
+    password = make_password(password)
+
+
+class AdminFactory(factory.Factory):
+    class Meta:
+        model = User
+
+    name = Faker().name()
+    email = Faker().email()
+    username = Faker().user_name()[0:10]
+    password = make_password(password)
+    is_staff = True
+    is_superuser = True
