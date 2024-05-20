8ad83140d9727cb026ae420dec41114b1f7af99e
Wissam Al-Kahwaji <wissamalkahwaji@gmail.com>
2024-05-20 21:58:09 +0300

    revert: forms

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -1,31 +1,25 @@
 from django.contrib.auth import forms as admin_forms
 from django.forms import EmailField
-from django import forms
 from django.utils.translation import gettext_lazy as _
-
 from .models import User
 
 
 class UserAdminChangeForm(admin_forms.UserChangeForm):
-    class Meta(admin_forms.UserChangeForm.Meta):
+    class Meta(admin_forms.UserChangeForm.Meta):  # type: ignore[name-defined]
         model = User
         field_classes = {"email": EmailField}
 
 
-class UserAdminCreationForm(forms.ModelForm):
+class UserAdminCreationForm(admin_forms.UserCreationForm):
     """
     Form for User Creation in the Admin Area.
     To change user signup, see UserSignupForm and UserSocialSignupForm.
     """
 
-    class Meta:
+    class Meta(admin_forms.UserCreationForm.Meta):  # type: ignore[name-defined]
         model = User
         fields = ("email",)
         field_classes = {"email": EmailField}
         error_messages = {
             "email": {"unique": _("This email has already been taken.")},
         }
-
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        return User.objects.create_user(email=user.email)
