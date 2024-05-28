f3afd9b1a6b6b2f9bdec7dbb30654177674e3daf
Wissam Al-Kahwaji <wissamalkahwaji@gmail.com>
2024-05-28 08:08:34 +0300

    feat: add send_email

---
+++ apps/utils/email.py
@@ -0,0 +1,18 @@
+from django.utils.html import strip_tags
+from django.core.mail import EmailMultiAlternatives
+from config.settings.base import APP
+
+
+def send_email(subject: str, to: list, html_message: str):
+    plain_message = strip_tags(html_message)
+    message = EmailMultiAlternatives(
+        subject=subject, body=plain_message, from_email=APP.name, to=to
+    )
+    message.attach_alternative(html_message, "text/html")
+
+    try:
+        message.send()
+        return True
+    except Exception as e:
+        print("An error occurred while sending email:", e)
+        return False
