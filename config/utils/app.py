88e909c67962355200d5c381d2ea80a54f5495ec
Wissam Al-Kahwaji <wissamalkahwaji@gmail.com>
2024-05-28 08:27:32 +0300

    chore: add Singleton Pattern on class App

--- config/utils/app.py
+++ config/utils/app.py
@@ -1,13 +1,29 @@
 import json
 from pathlib import Path
 import os
+import threading
 
 
 class App:
+    _instance = None
+    _lock = threading.Lock()
+    _data = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            with cls._lock:
+                if not cls._instance:
+                    cls._instance = super(App, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
+
     def __init__(self):
+        if not hasattr(self, "initialized"):
+            self.initialized = True
+            self.setup()
+
+    def setup(self):
         self.BASE_DIR = Path(__file__).resolve().parent.parent.parent
         self._json_file = os.path.join(self.BASE_DIR, "config/app.json")
-        self._data = None
         self.read_file()
 
     def read_file(self):
