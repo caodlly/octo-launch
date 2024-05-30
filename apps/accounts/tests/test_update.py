f83b63be45f950b42844c044343825622d66811e
Wissam Al-Kahwaji <wissamalkahwaji@gmail.com>
2024-05-30 08:09:20 +0300

    test: add check_password

--- apps/accounts/tests/test_update.py
+++ apps/accounts/tests/test_update.py
@@ -3,6 +3,8 @@ from django.urls import reverse
 from apps.utils.generate import generate_register, generate_image
 from apps.users.factories import password
 from rest_framework import status
+from apps.users.models import User
+from django.contrib.auth.hashers import check_password
 
 
 @pytest.mark.django_db
@@ -21,3 +23,6 @@ def test_update_200(client, user):
     assert response.data["name"] == data_register["name"]
     assert response.data["username"] == data_register["username"]
     assert response.data["avatar"].split("/")[3] != "avatar.svg"
+
+    _user = User.objects.get(id=user.id)
+    assert check_password(data_register["password"], _user.password)
