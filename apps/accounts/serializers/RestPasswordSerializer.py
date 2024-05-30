2543d912b44e11aeb7aa1257090c67bee0ea8dc6
Wissam Al-Kahwaji <wissamalkahwaji@gmail.com>
2024-05-30 08:03:35 +0300

    feat: add email to reset password serializer & somefix

--- apps/accounts/serializers/RestPasswordSerializer.py
+++ apps/accounts/serializers/RestPasswordSerializer.py
@@ -5,17 +5,25 @@ from apps.accounts.serializers import UpdateUserSerializer
 
 
 class ResetPasswordSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(max_length=128, min_length=5, write_only=True)
-    password2 = serializers.CharField(max_length=128, min_length=5, write_only=True)
+    password = serializers.CharField(
+        max_length=128, min_length=5, write_only=True, required=True
+    )
+    password2 = serializers.CharField(
+        max_length=128, min_length=5, write_only=True, required=True
+    )
+    email = serializers.EmailField(write_only=True, required=True)
 
     class Meta:
         model = VerificationCode
-        fields = ["key", "password", "password2"]
+        fields = ["key", "email", "password", "password2"]
+        extra_kwargs = {"key": {"required": True}}
 
     def validate(self, data, *args, **kwargs):
         key = data["key"]
+        email = data["email"]
+
         try:
-            model = self.Meta.model.objects.get(key=key)
+            model = self.Meta.model.objects.get(key=key, user__email=email)
             user = model.user
         except self.Meta.model.DoesNotExist:
             raise VerificationKeyError()
