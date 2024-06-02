a600b1e233b4580b99d764a255ee42f7c156d06a
Wissam Al-Kahwaji <wissamalkahwaji@gmail.com>
2024-06-02 22:51:11 +0300

    feat: create docs file

---
+++ docs/conf.py
@@ -0,0 +1,67 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+import datetime
+import django
+
+sys.path.insert(0, os.path.abspath(".."))
+os.environ["DATABASE_URL"] = "sqlite:///readthedocs.db"
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.development")
+django.setup()
+
+
+def get_app():
+    from config.utils.app import App
+
+    return App()
+
+
+app = get_app()
+
+current_year = datetime.datetime.now().year
+if 2024 == current_year:
+    date = current_year
+else:
+    date = "2024-{}".format(current_year)
+
+
+project = app.name
+copyright = f"{date}, {app.author__name}"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx_rtd_theme",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.doctest",
+    "sphinx.ext.intersphinx",
+    "sphinx_copybutton",
+    "sphinxext.opengraph",
+]
+
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3/", None),
+    "django": (
+        "https://docs.djangoproject.com/en/4.2/",
+        "https://docs.djangoproject.com/en/4.2/_objects/",
+    ),
+}
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
---
+++ docs/index.rst
@@ -0,0 +1,8 @@
+.. Django-EaseLaunch documentation master file, created by
+   sphinx-quickstart on Sat Jun  1 21:34:55 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Django-EaseLaunch documentation!
+=============================================
+
