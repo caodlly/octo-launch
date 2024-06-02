0e4cab5c7181d4cbfb7dd970b8ec9ab7c7411b89
Wissam Al-Kahwaji <wissamalkahwaji@gmail.com>
2024-06-02 22:53:36 +0300

    test: add test_me

---
+++ apps/accounts/tests/test_me.py
@@ -0,0 +1,44 @@
+import pytest
+from django.urls import reverse
+from apps.users.factories import password
+from rest_framework import status
+from apps.accounts.serializers import UserSerializer
+
+
+@pytest.mark.django_db
+def test_me_get_200(client, user, factory):
+    client.login(email=user.email, password=password)
+    response = client.get(reverse("user_me"))
+
+    serializer = UserSerializer(
+        user, context={"request": factory.get(reverse("user_me"))}
+    )
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data["id"] == serializer.data["id"]
+    assert response.data["username"] == serializer.data["username"]
+    assert response.data["avatar"] == serializer.data["avatar"]
+    assert response.data["name"] == serializer.data["name"]
+    assert response.data["email"] == serializer.data["email"]
+
+
+@pytest.mark.django_db
+def test_me_get_403(client, factory):
+    response = client.get(reverse("user_me"))
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_me_post_200(client, user, factory):
+    client.login(email=user.email, password=password)
+    response = client.post(reverse("user_me"))
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data["status"] is True
+
+
+@pytest.mark.django_db
+def test_me_post_200_1(client, user, factory):
+    response = client.post(reverse("user_me"))
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data["status"] is False
