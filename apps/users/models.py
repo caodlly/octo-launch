766dc6ede1fbc9dd6bfafcab60c9bda25d5d34df
Wissam Al-Kahwaji <wissamalkahwaji@gmail.com>
2024-06-09 21:14:19 +0300

    fix: create signals.py for fix cyclic import

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,12 +1,8 @@
-from django.db.models import signals
-from django.dispatch import receiver
-from apps.utils.image import resize_image
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
 from typing import ClassVar
 from .managers import UserManager
-from apps.utils.generate import generate_code
 
 
 class User(AbstractUser):
@@ -31,32 +27,3 @@ class User(AbstractUser):
         indexes = [
             models.Index(fields=["email"]),
         ]
-
-
-@receiver(signals.pre_save, sender=User)
-def pre_save_user(sender: User, instance: User, **kwargs):
-    try:
-        if instance.pk:
-            old_instance = sender.objects.get(pk=instance.pk)
-
-            if instance.avatar != old_instance.avatar:
-                instance.avatar = resize_image(instance.avatar, 450, 450)
-
-            if instance.email != old_instance.email:
-                instance.email_verified = False
-        if not instance.username:
-            instance.username = instance.email.split("@")[0] + "_" + generate_code()
-    except Exception as e:
-        raise Exception(f"Error in pre_save signal: {e}")
-
-
-@receiver(signals.post_save, sender=User)
-def post_save_user(sender: User, instance: User, created, **kwargs):
-    try:
-        if not instance.email_verified:
-            from apps.accounts.tasks import send_verification_email
-
-            send_verification_email.delay(instance.email)
-
-    except Exception as e:
-        raise Exception(f"Error in post_save signal: {e}")
