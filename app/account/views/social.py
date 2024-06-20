b8ed03c6ff413deaa30406890b33fbffdac9b158
abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-06-20 10:21:01 +0300

    add OAuthLogin generic view, inside this view we can handle multiple providers
    Signed-off-by: abdelrahman hassan <abdelrahman_hassan31@yahoo.com>

---
+++ app/account/views/social.py
@@ -0,0 +1,51 @@
+"""oauth_factory.py: Factory function to return the appropriate OAuth handler based on the provider."""
+from rest_framework.views import APIView
+from rest_framework.response import Response
+import logging
+from django.contrib.auth import get_user_model
+from rest_framework import status
+from app.utils.oauth_factory import get_oauth_handler
+
+User = get_user_model()
+logger = logging.getLogger(__name__)
+
+
+class OAuthLoginView(APIView):
+    """
+    Handle Google login, validate access token, register/login user, and return JWT.
+    """
+    permission_classes = []
+
+    def post(self, request, provider, *args, **kwargs):
+        access_token = self.extract_token(request)
+        if not access_token:
+            return Response({'error': 'Access token not provided or improperly formatted.'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        handler = get_oauth_handler(provider, access_token)
+        if not handler:
+            return Response({'error': 'Unsupported provider.'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        user_info = handler.get_user_info()
+        if not user_info:
+            return Response({'error': f'Invalid or expired {provider} access token.'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        user = handler.get_or_create_user(user_info)
+        if not user:
+            return Response({'error': 'User could not be created or retrieved.'},
+                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        tokens = handler.get_tokens_for_user(user)
+        return Response(tokens, status=status.HTTP_200_OK)
+
+    @staticmethod
+    def extract_token(request):
+        """ Extracts the bearer token from the Authorization header. """
+        auth_header = request.headers.get('Authorization', '')
+        if 'Bearer ' in auth_header:
+            return auth_header.split('Bearer ')[1].strip()
+        else:
+            logger.error(f"Authorization header malformed or missing: {auth_header}")
+            return auth_header
