4a21760d3795b3738a666d266b671a6a42b4e8c8
abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-06-20 10:18:08 +0300

    add BaseOAuthHandler class
    Signed-off-by: abdelrahman hassan <abdelrahman_hassan31@yahoo.com>

---
+++ app/utils/base_oauth_handler.py
@@ -0,0 +1,51 @@
+"""base_oauth_handler.py: Contains the BaseOAuthHandler class that is inherited by the provider-specific classes."""
+import requests
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import get_user_model
+from app.utils.generate import generate_code
+import logging
+
+User = get_user_model()
+
+logger = logging.getLogger(__name__)
+
+
+class BaseOAuthHandler:
+    USER_INFO_URL = None
+    EMAIL_FIELD = 'email'
+    NAME_FIELD = 'name'
+
+    def __init__(self, access_token):
+        self.access_token = access_token
+
+    def get_user_info(self):
+        headers = {'Authorization': f'Bearer {self.access_token}'}
+        try:
+            response = requests.get(self.USER_INFO_URL, headers=headers)
+            response.raise_for_status()
+            return response.json()
+        except requests.RequestException as e:
+            logger.error(f"Failed to fetch user info: {e}")
+        return None
+
+    def get_or_create_user(self, user_info):
+        email = user_info.get(self.EMAIL_FIELD, '')
+        username = email.split("@")[0] + "_" + generate_code(4)
+        name = user_info.get(self.NAME_FIELD, username)
+        try:
+            user, _ = User.objects.get_or_create(
+                email=email,
+                defaults={'username': username, 'name': name}
+            )
+            return user
+        except Exception as e:
+            logger.error(f"Error creating or retrieving user: {e}")
+            return None
+
+    @staticmethod
+    def get_tokens_for_user(user):
+        refresh = RefreshToken.for_user(user)
+        return {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        }
