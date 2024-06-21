15ad1284793ff09900e62941e554d02a5990760e
abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-06-22 01:25:35 +0300

    return logger back
    Signed-off-by: abdelrahman hassan <abdelrahman_hassan31@yahoo.com>

--- app/account/apps.py
+++ app/account/apps.py
@@ -4,5 +4,3 @@ from django.apps import AppConfig
 class AccountConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "app.account"
-    label = "app_account"  # in order to avoid conflict with the allauth app
-    verbose_name = "builtInAccount"
--- config/settings/base.py
+++ config/settings/base.py
@@ -64,7 +64,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "app.user",
-    "app.account.apps.AccountConfig",
+    "app.account",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
