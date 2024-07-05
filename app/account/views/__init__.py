3903a1f8dde14470ce30c44022a404113c3d7ec0
Wissam Al-Kahwaji <wissamalkahwaji@gmail.com>
2024-07-05 12:36:49 +0300

    chore: add OAuthLoginView

--- app/account/views/__init__.py
+++ app/account/views/__init__.py
@@ -4,6 +4,7 @@ from .register import Register  # noqa: F401
 from .me import Me  # noqa: F401
 from .update import UpdateProfile  # noqa: F401
 from .email_verified import SendEmailCodeVerify, EmailCodeVerify  # noqa: F401
+from .social import OAuthLoginView  # noqa: F401
 from .reset_password import (
     SendCodeRestPassword,  # noqa: F401
     VerifyCodeResetPassowrd,  # noqa: F401
